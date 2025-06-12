8434c685c911881f83b856e40fdfb66513fbba13
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2025-06-12 06:28:14 -0300

    primeiro commit e config

---
+++ src/global/themes.tsx
@@ -0,0 +1,12 @@
+export const themas = {
+    colors:{
+        primary:'#878af6',
+        secondary:'@ffffff',
+        lightGray:'#f5f5f5',
+        gray:'gray',
+        black:'#000000',
+        white:'#ffffff',
+        bgScreen:'#f1f7fa',
+        bgColor:'#a4c639',
+    }
+}
