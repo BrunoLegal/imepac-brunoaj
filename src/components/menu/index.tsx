449018959afb4124c908ebfb15b3e9814648ea8c
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2025-06-12 07:46:40 -0300

    adiciona componente menu

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,36 @@
+import { useNavigation } from "@react-navigation/native";
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { RootStackParamList } from "../../pages/AppNavigation";
+import { TouchableOpacity } from "react-native";
+import {style } from "./styles";
+import {View, Text} from "react-native";
+
+type NavigationProps = DrawerScreenProps<RootStackParamList>;
+
+export default function Menu() {
+    const navigation = useNavigation<NavigationProps["navigation"]>();
+
+    return (
+        <View style={style.menuContainer}>
+            <TouchableOpacity
+                
+                onPress={() => navigation.navigate("Home")}
+            >
+                <Text style={style.menuItem}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                
+                onPress={() => navigation.navigate("Login")}
+            >
+                <Text style={style.menuItem}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                
+                onPress={() => navigation.navigate("Cadastro")}
+            >
+                <Text style={style.menuItem}>Cadastro</Text>
+            </TouchableOpacity>
+            
+        </View>
+    );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,10 +5,12 @@ import {
     View,
 } from 'react-native';
 import {style} from "./styles";
+import Menu from "../../components/menu";
 
 export default function handleHome() {
     return (
         <View style={style.container}>
+            <Menu />
             <Text>Home</Text>
         </View>
     );
